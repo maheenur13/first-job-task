bc8a72f1f32e1df09f469792b0a071575e93b2cc
Mahee Nur <differentmahee13@gmail.com>
2021-09-11 19:41:58 +0600

    final push

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -5,7 +5,7 @@ import SlideBar from '../SlideBar/SlideBar';
 const Blog = () => {
     return (
         <Col  className=" container mx-auto">
-            <Row className="border">
+            <Row className="">
                 <h1 className="text-center w-100">LATEST BLOGS</h1>
                 <h5 className="text-center w-100 mx-auto my-3">This is a place devoted to giving you deeper insight into the news, trends, people and technology behind Bing.</h5>
             </Row>
